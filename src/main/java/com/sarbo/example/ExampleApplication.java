9ce84ad71972a8288431b088be5a4eee57e756d2
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-03-18 00:07:06 +0530

    Add actuator and remove example artifacts
    Add Spring Boot Actuator dependency to pom.xml and remove example/demo code and resources. Deleted ApplicationConfig, MyFirstClass, MyFirstService, custom property files and environment YAMLs (under src/main/resources) as well as their compiled copies in target/classes. Simplified ExampleApplication.main to call SpringApplication.run(...) instead of manually setting a profile and fetching beans. This cleans up sample/demo components while enabling actuator endpoints.

--- src/main/java/com/sarbo/example/ExampleApplication.java
+++ src/main/java/com/sarbo/example/ExampleApplication.java
@@ -10,21 +10,8 @@ import java.util.Collections;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
-		var ctx = app.run(args);
+		SpringApplication.run(ExampleApplication.class);
 
-//		var ctx = SpringApplication.run(ExampleApplication.class, args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-//		System.out.println(ctx.getBeanDefinitionNames());
-//		System.out.println(myFirstService.getJavaVersion());
-//		System.out.println(myFirstService.getOsName());
-//		System.out.println(myFirstService.readProp());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomProperty2());
-		System.out.println(myFirstService.getCustomProperty3());
 	}
 
 
